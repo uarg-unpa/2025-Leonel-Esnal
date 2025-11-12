5dfa56002473e7c70f4247cd92da87f830cb72f5
Leonel-Esnal <esnal2018@gmail.com>
2025-11-12 11:29:31 -0300

    Le agregue un menu a la main

--- Parciales/TestRectangulo.java
+++ Parciales/TestRectangulo.java
@@ -7,7 +7,7 @@ public class TestRectangulo {
         System.out.println("Ingrese la base del primer rectangulo");
         double base1 = sc.nextDouble();
 
-        System.out.println("Ingrese la altura del prrimer rectangulo");
+        System.out.println("Ingrese la altura del primer rectangulo");
         double altura1 = sc.nextDouble();
 
         sc.nextLine();
@@ -24,14 +24,46 @@ public class TestRectangulo {
 
         Rectangulo R2 = new Rectangulo(base2, altura2);
 
-        System.out.println("El area del primer rectagulo es: " + R1.CalcularArea());
-        
-        System.out.println("El perimetro del segundo rectangulo es: " + R2.CalcularPerimetro());
+        int opcion;
 
-        System.out.println("El primer rectangulo tiene mayor area que el segundo: " + R1.esMayorArea(R2));
+        do{
+            System.out.println("=== Menu ===");
+            System.out.println("1. Mostrar el area del R1");
+            System.out.println("2. Mostrar el perimetro de R2");
+            System.out.println("3. Mostrar cual rectagulo tiene mayor area");
+            System.out.println("0. Salir del menu");
+            opcion = sc.nextInt();
 
-        System.out.println(R1);
-        System.out.println(R2);
+            switch (opcion) {
+                case 1:
+                    System.out.println("El area del R1 es: " + R1.CalcularArea());
+                    break;
+            
+                case 2:
+                    System.out.println("El perimetro " + R2.CalcularPerimetro());
+                    break;
+
+                case 3:
+                    if (R1.esMayorArea(R2)) {
+                        System.out.println("R1 tiene mayor area que R2");
+                    }
+                    else if (R2.esMayorArea(R1)) {
+                        System.out.println("R2 tiene mayor area que R1");
+                    }
+                    else{
+                        System.out.println("Tienen la misma area");
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Saliendo del menu...");
+                    break;
+
+                default:
+                System.out.println("Elije una opcion valida");
+                    break;
+            }
+        }while(opcion != 0);
 
         sc.close();
     }
