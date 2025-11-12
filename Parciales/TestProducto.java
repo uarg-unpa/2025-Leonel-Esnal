a359de1bcfd912b9e11e7ba17c76772b8b5dc6cf
Leonel-Esnal <esnal2018@gmail.com>
2025-11-12 10:54:59 -0300

    Corrección con un menú

--- Parciales/TestProducto.java
+++ Parciales/TestProducto.java
@@ -15,7 +15,7 @@ public class TestProducto {
 
          sc.nextLine();
 
-        Producto producto1 = new Producto(nombre1, precio1, cantidadEnStock1);
+        Producto p1 = new Producto(nombre1, precio1, cantidadEnStock1);
 
          System.out.println("Ingrese nombre del segundo producto");
         String nombre2 = sc.nextLine();
@@ -27,20 +27,52 @@ public class TestProducto {
         int cantidadEnStock2 = sc.nextInt();
 
         sc.nextLine();
-        Producto producto2 = new Producto(nombre2, precio2, cantidadEnStock2);
+        Producto p2 = new Producto(nombre2, precio2, cantidadEnStock2);
 
-        producto1.Vender(30);
-        System.out.println(producto1);
-        producto2.aumentarStock(10);
-        System.out.println(producto2);
+        int opcion;
+        do{
+            System.out.println("=== MENU ===");
+            System.out.println("1. Vender stock de p1");
+            System.out.println("2. Aumentar el stock de p2");
+            System.out.println("3. mostrar el producto con mas stock");
+            System.out.println("0. salir del menu");
+            opcion = sc.nextInt();
 
-        int diferente = producto1.tieneMasStock(producto2);
-        if (diferente > 0) {
-            System.out.println("El producto con mayor stock es: " + producto1.getNombre());
-        }else if(diferente < 0){
-            System.out.println("El producto con mayor stock es: " + producto2.getNombre());
-        }
-        System.out.println("Ambos productos tinen la misma cantidad");
+            switch (opcion) {
+                case 1:
+                    System.out.println("Ingrese la cantidad a vender: ");
+                    int cantidad = sc.nextInt();
+                    p1.Vender(cantidad);
+                    System.out.println("Se vendio la cantidad " + cantidad);
+                    break;
+            
+                case 2:
+                    p2.aumentarStock(10);
+                    break;
+
+                case 3:
+                    int diferencia = p1.tieneMasStock(p2);
+                    if (diferencia > 0) {
+                        System.out.println(p1.getNombre() + " tiene mas stock.");
+                    }
+                    else if (diferencia < 0) {
+                        System.out.println(p2.getNombre() + " tiene mas stock.");
+                    }
+                    else{
+                        System.out.println("Ambos productos tienen el mismo stock");
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Saliendo del menu.....");
+                    break;
+
+                default:
+                System.out.println("Elija una opcion valida");
+                    break;
+            }
+
+        }while(opcion != 0);
         sc.close();
     }  
 }
