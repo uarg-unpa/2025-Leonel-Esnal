008b5baa47c9d1ffebd92f7a3ac59b7b0b1777ba
Leonel-Esnal <esnal2018@gmail.com>
2025-11-19 11:32:07 -0300

    Actividades antes del recuperatorio

---
+++ PalabrasClave/Cola.java
@@ -0,0 +1,64 @@
+public class Cola{
+    private final int max = 10;
+    private int frente, ultimo;
+    private String [] elemento;
+
+    public Cola(){
+        frente = 0;
+        ultimo = 0;
+        elemento = new String[max];
+    }
+
+    public boolean estaVacia(){
+        return (ultimo == 0);
+    }
+
+    public boolean estaLleno(){
+        return (ultimo == max);
+    }
+
+    public void insertar(String Llamada){
+        elemento [ultimo] = Llamada;
+        ultimo++;    
+    }
+
+    public String borrar(){
+        String aux = elemento [frente];
+        for(int i = 0; i < ultimo - 1; i++ ){
+            elemento[i] = elemento [i + 1];
+        }
+        ultimo--;
+        return aux;
+    }
+
+    public Cola filtrarLlamadas(String palabraClave){
+        Cola NuevaCola = new Cola();
+        Cola aux = new Cola();
+
+        int clave = palabraClave.length();
+
+        while (!estaVacia()) {
+            String palabra = borrar();
+            boolean coincide = false;
+
+            for(int i = 0; i < palabra.length() - clave; i++ ){
+                String clave2 = palabra.substring(i, i + clave);
+
+                if (clave2.equals(palabraClave)) {
+                    coincide = true;
+                }
+            }
+
+            if (coincide) {
+                NuevaCola.insertar(palabra);
+            }
+            aux.insertar(palabra);
+        
+        }
+        while (!aux.estaVacia()) {
+            insertar(aux.borrar());
+        }
+
+        return NuevaCola;
+    }
+}
